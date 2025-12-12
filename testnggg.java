34a7b9df18c021d0ddc816c32af2c4cca9520a69
vashistavashi82-star <vashistavashi82@gmail.com>
2025-12-12 12:02:09 +0530

    string reverse

---
+++ testnggg.java
@@ -0,0 +1,11 @@
+public class testnggg {
+    
+public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder
+        ("Hello");
+        sb.append(" World");
+        System.out.println(sb.reverse());
+}
+}
+    
+
